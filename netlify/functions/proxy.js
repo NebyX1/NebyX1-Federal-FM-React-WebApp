92f5e635d48bcfdc4485999500c0b1dbb3f41aa5
NebyX1 <blackcat_neme@hotmail.com>
2024-05-28 15:43:16 -0300

    lets try this proxy again

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -2,13 +2,14 @@ const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
   const response = await fetch('http://usa15.ciudaddigital.com.uy:8040/FederalFM');
-  const body = await response.body;
+  const body = await response.buffer();
 
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'audio/mpeg',
     },
-    body: body,
+    body: body.toString('base64'),
+    isBase64Encoded: true,
   };
 };
