17bbe5060fa98fdd8bb9c4187da1cb3fbd929693
NebyX1 <blackcat_neme@hotmail.com>
2024-05-28 16:19:44 -0300

    lets try this proxy on render fixed

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-const express = require('express');
-const request = require('request');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(express.static('dist'));
+// Sirve archivos estáticos de la carpeta 'dist'
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/api/FederalFM', (req, res) => {
-  const url = 'http://usa15.ciudaddigital.com.uy:8040/FederalFM';
-  req.pipe(request(url)).pipe(res);
+// Configura el proxy para las solicitudes de audio
+app.use('/api', createProxyMiddleware({
+  target: 'http://usa15.ciudaddigital.com.uy:8040',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': '', // Remueve el prefijo /api
+  },
+}));
+
+// Para todas las demás rutas, sirve el archivo index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
