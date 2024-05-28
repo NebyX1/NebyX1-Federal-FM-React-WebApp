e22ec68031b2e44d22f0cfaba0db18f90eb7dd6b
NebyX1 <blackcat_neme@hotmail.com>
2024-05-28 16:00:14 -0300

    lets try this proxy again but different

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -2,14 +2,15 @@ const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
   const response = await fetch('http://usa15.ciudaddigital.com.uy:8040/FederalFM');
-  const body = await response.buffer();
+  const buffer = await response.buffer();
 
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'audio/mpeg',
+      'Access-Control-Allow-Origin': '*',
     },
-    body: body.toString('base64'),
     isBase64Encoded: true,
+    body: buffer.toString('base64'),
   };
 };
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://usa15.ciudaddigital.com.uy:8040',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
